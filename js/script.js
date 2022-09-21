a047d9829df27fbe869c76743e21b3cc30c232f9
hakonbjork <hakon1307@gmail.com>
2022-09-21 10:23:16 +1000

    bug fixes before review and fade in transition

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ document.addEventListener("readystatechange", function (event) {
     // console.log("yeah page is ready");
   }
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      // entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
