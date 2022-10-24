f2b45c981a206da7afc1c94ba7b99e39c896922f
Haakon Bjoerkum <hakon1307@gmail.com>
2022-10-24 16:47:06 +1000

    booking popup and responsive

--- js/booking.js
+++ js/booking.js
@@ -4,6 +4,13 @@ document.addEventListener("readystatechange", function (event) {
     addValidateFormInputListener();
     addKeyDownListeners();
     addCalendarInputListeners();
+
+    document
+      .querySelector("#confirm-booking .confirm-button")
+      .addEventListener("click", storeValuesSwitchPage);
+    document
+      .querySelector("#confirm-booking .change-button")
+      .addEventListener("click", hidePopupEnableButtons);
   }
 });
 
@@ -24,11 +31,12 @@ function addValidateFormInputListener() {
 
       if (complete) {
         if (validateFormContent()) {
-          const button = document.querySelector("#booking-form button");
-          button.innerHTML = "Booking Completed ✅";
-          button.setAttribute("disabled", "true");
-          storeValues();
-          window.location.href = "./booking-confirmation.html";
+          document
+            .querySelectorAll("#booking-form input, #booking-form button")
+            .forEach((el) => el.setAttribute("disabled", true));
+          document.querySelector("#booking-form button").innerHTML =
+            "Confirming...";
+          showConfirmBookingPopup();
         }
       }
       event.preventDefault();
@@ -41,6 +49,7 @@ function addKeyDownListeners() {
   for (let input of formInputs) {
     input.addEventListener("keydown", function (event) {
       input.classList.remove("form-error");
+      event.preventDefault();
     });
   }
 }
@@ -86,8 +95,11 @@ function validateFormContent() {
   return contentValid;
 }
 
-// Store values in localStorage for later use
-function storeValues() {
+function showConfirmBookingPopup() {
+  const confirmBookingElement = document.querySelector("#confirm-booking");
+  confirmBookingElement.classList.remove("hidden");
+  confirmBookingElement.scrollIntoView();
+
   const name = document.querySelector("#form-name").value;
   const number = document.querySelector("#form-number").value;
   const mail = document.querySelector("#form-mail").value;
@@ -95,12 +107,44 @@ function storeValues() {
   const startDate = document.querySelector("#form-start-date").value;
   const endDate = document.querySelector("#form-end-date").value;
 
-  console.log(name);
-  console.log(number);
-  console.log(mail);
-  console.log(numGuests);
-  console.log(startDate);
-  console.log(endDate);
+  document.querySelector(
+    "#confirm-booking #details p:nth-child(1)"
+  ).innerHTML = `Name: ${name}`;
+  document.querySelector(
+    "#confirm-booking #details p:nth-child(2)"
+  ).innerHTML = `Number: ${number}`;
+  document.querySelector(
+    "#confirm-booking #details p:nth-child(3)"
+  ).innerHTML = `Mail: ${mail}`;
+  document.querySelector(
+    "#confirm-booking #details p:nth-child(4)"
+  ).innerHTML = `Number of guests: ${numGuests}`;
+  document.querySelector(
+    "#confirm-booking #details p:nth-child(5)"
+  ).innerHTML = `Date of arrival: ${startDate}`;
+  document.querySelector(
+    "#confirm-booking #details p:nth-child(6)"
+  ).innerHTML = `Date of departure: ${endDate}`;
+}
+
+function hidePopupEnableButtons(event) {
+  document.querySelector("#confirm-booking").classList.add("hidden");
+  document.querySelector("#booking-form button").innerHTML = "Send booking";
+  document
+    .querySelectorAll("#booking-form input, #booking-form button")
+    .forEach((el) => el.removeAttribute("disabled"));
+
+  event.preventDefault();
+}
+
+// Store values in localStorage for later use
+function storeValuesSwitchPage(event) {
+  const name = document.querySelector("#form-name").value;
+  const number = document.querySelector("#form-number").value;
+  const mail = document.querySelector("#form-mail").value;
+  const numGuests = document.querySelector("#form-num-guests").value;
+  const startDate = document.querySelector("#form-start-date").value;
+  const endDate = document.querySelector("#form-end-date").value;
 
   localStorage.setItem("booking-name", name);
   localStorage.setItem("booking-number", number);
@@ -108,4 +152,18 @@ function storeValues() {
   localStorage.setItem("booking-numGuests", numGuests);
   localStorage.setItem("booking-startDate", startDate);
   localStorage.setItem("booking-endDate", endDate);
+
+  window.location.href = "./booking-confirmation.html";
+
+  event.preventDefault();
+}
+
+/* Used to make development and testing faster */
+function fillFields() {
+  document.querySelector("#form-name").value = "Hans Hansen";
+  document.querySelector("#form-number").value = 23456;
+  document.querySelector("#form-mail").value = "hans@hansen.com";
+  document.querySelector("#form-num-guests").value = 3;
+  document.querySelector("#form-start-date").value = "2023-02-27";
+  document.querySelector("#form-end-date").value = "2023-03-27";
 }
